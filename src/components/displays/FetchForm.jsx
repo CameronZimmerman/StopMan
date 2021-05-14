af404c8d8700bab322cf5f68a2d191beaf2d01d2
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-05-14 13:25:29 -0700

    create FetchForm.jsx

--- src/components/displays/FetchForm.jsx
+++ src/components/displays/FetchForm.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function FetchForm(props) {
+function FetchForm({method, url, body, onChange, onSubmit}) {
   return (
-    <div>
-      FetchForm
-    </div>
+    <>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="url">
+          <input id="url" value={url} type="text"/>
+          <button></button>
+        </label>
+        <div onChange={event => onChange(event)}>
+          <input type="radio" checked={method === "GET"} value="GET" defaultChecked name="method"/> GET
+          <input type="radio" checked={method === "POST"} value="POST" name="method"/> POST
+          <input type="radio" checked={method === "DELETE"} value="DELETE" name="method"/> DELETE
+          <input type="radio" checked={method === "PUT"} value="PUT" name="method"/> PUT
+        </div>
+        <label htmlFor="body">
+          <textarea id="body" rows="4" cols="50" value={body} onChange={onChange}></textarea>
+        </label>
+      </form>
+    </>
   )
 }
 
 FetchForm.propTypes = {
+  method: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 
 }
 
